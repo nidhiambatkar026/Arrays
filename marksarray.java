22bc52b32c6455355204e45d6c6369539c3f509c
nidhiambatkar026 <nidhiambatkar@gmail.com>
2025-01-26 12:43:51 +0530

    first array code

--- marksarray.java
+++ marksarray.java
@@ -1,3 +1,12 @@
 public class marksarray {
-    
+    public static void main(String args[]){
+        int marks[] = new int[3];
+        marks[0] = 97;
+        marks[1] = 93;
+        marks[2] = 96;
+
+        for(int i= 0; i<3; i++){
+            System.out.println(marks[i]);
+        }
+    }
 }
